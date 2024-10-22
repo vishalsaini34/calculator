28bcc1bfbd86590ac362357a94828c3cabb4ff3d
vishalsaini34 <vishalmzn34@gmail.com>
2024-10-22 16:07:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    let expression = document.getElementById('display').value;
+    try {
+        let result = eval(expression);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
